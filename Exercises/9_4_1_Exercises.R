5ec622d4503d56f482d29635480de197db9b2737
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2024-07-24 21:05:58 +0200

    too tired to understand anything

--- Exercises/9_4_1_Exercises.R
+++ Exercises/9_4_1_Exercises.R
@@ -20,20 +20,19 @@ ggplot(mpg) +
 ggplot(mpg) + 
     geom_point(aes(x = displ, y = hwy)) +
     facet_grid(. ~ cyl)
-
-Take the first faceted plot in this section:
+# Take the first faceted plot in this section:
     
     ggplot(mpg) + 
     geom_point(aes(x = displ, y = hwy)) + 
     facet_wrap(~ cyl, nrow = 2)
 
-What are the advantages to using faceting instead of the color aesthetic? What are the disadvantages? How might the balance change if you had a larger dataset?
+# What are the advantages to using faceting instead of the color aesthetic? What are the disadvantages? How might the balance change if you had a larger dataset?
     
-    Read ?facet_wrap. What does nrow do? What does ncol do? What other options control the layout of the individual panels? Why doesn’t facet_grid() have nrow and ncol arguments?
+# Read ?facet_wrap. What does nrow do? What does ncol do? What other options control the layout of the individual panels? Why doesn’t facet_grid() have nrow and ncol arguments?
     
-    Which of the following plots makes it easier to compare engine size (displ) across cars with different drive trains? What does this say about when to place a faceting variable across rows or columns?
-    
-    ggplot(mpg, aes(x = displ)) + 
+# Which of the following plots makes it easier to compare engine size (displ) across cars with different drive trains? What does this say about when to place a faceting variable across rows or columns?
+
+ggplot(mpg, aes(x = displ)) + 
     geom_histogram() + 
     facet_grid(drv ~ .)
 
@@ -41,9 +40,35 @@ ggplot(mpg, aes(x = displ)) +
     geom_histogram() +
     facet_grid(. ~ drv)
 
-Recreate the following plot using facet_wrap() instead of facet_grid(). How do the positions of the facet labels change?
+# Recreate the following plot using facet_wrap() instead of facet_grid(). How do the positions of the facet labels change?
     
-    ggplot(mpg) + 
+ggplot(mpg) + 
     geom_point(aes(x = displ, y = hwy)) +
     facet_grid(drv ~ .)
 
+
+# 4
+
+# facet
+ggplot(mpg) + 
+    geom_point(aes(x = displ, y = hwy)) + 
+    facet_wrap(~ class, nrow = 3) # how many rows should be created for the facet
+
+# color
+ggplot(mpg) + 
+    geom_point(aes(x = displ, y = hwy, color = class))
+
+# both
+ggplot(mpg) + 
+    geom_point(
+        aes(x = displ, y = hwy, color = class), 
+        show.legend = FALSE) + 
+    facet_wrap(~ class, nrow = 2)
+
+# highlighting
+ggplot(mpg, aes(x = displ, y = hwy)) + 
+    geom_point(color = "gray") +
+    geom_point(
+        data = mpg %>% filter(class == "compact"),
+        color = "pink"
+    )
